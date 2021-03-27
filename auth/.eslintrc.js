2abbb97ce599e4cd3c0e25558b52e50729d12dc7
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-03-27 21:43:03 +0300

    refactor(auth): update eslint and prettier configs

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -1,9 +1,23 @@
 module.exports = {
-    extends: "airbnb-typescript-prettier",
+    extends: [
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    plugins: ['import', 'prettier', '@typescript-eslint'],
     parserOptions: {
         project: "./tsconfig.json",
         tsconfigRootDir: __dirname,
         sourceType: "module",
+    },
+    rules: {
+        quotes: [
+            'error',
+            'single',
+            {
+                avoidEscape: true, allowTemplateLiterals: false
+            }
+        ]
     },
     ignorePatterns: ['.eslintrc.js'],
 };
