e071482b7c319cef37cfa053874e02d8091e9725
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-03-31 22:26:29 +0300

    chore(auth): removed unnecessary console-log

--- auth/src/utils/email-sender/nodemailer-email-api.ts
+++ auth/src/utils/email-sender/nodemailer-email-api.ts
@@ -35,13 +35,12 @@ export default class NodemailerEmailApi extends EmailSenderEmailApi {
 
   protected async sendEmail(args: EmailApiSendEmailArgs): Promise<void> {
     const { toEmail } = args;
-    const res = await this.transporter.sendMail({
+
+    await this.transporter.sendMail({
       from: 'noreply@destur.com',
       to: toEmail,
       subject: 'My first email',
       text: 'This is first test email'
     });
-
-    console.log(res);
   }
 }
