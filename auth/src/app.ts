a6896122546e696c44ce6dba7d1de26625753604
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-05-09 14:07:41 +0300

    feat(auth): Add cors configuration

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
+import cors from 'cors';
 
 import { signUpRouter } from './routes';
 import { errorHandler } from './middlewares';
 
 const app = express();
 
+const options: cors.CorsOptions = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: 'http://localhost:3000/',
+  preflightContinue: false
+};
+
+app.use(cors(options));
 app.use(json());
 app.use(signUpRouter);
 app.use(errorHandler);
