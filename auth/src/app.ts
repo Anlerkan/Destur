483c44cc6680f5cb85716b3b66d727b444e43988
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-05-09 14:20:43 +0300

    fix(cors): origin fixed

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -12,7 +12,7 @@ const options: cors.CorsOptions = {
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: 'http://localhost:3000/',
+  origin: '*',
   preflightContinue: false
 };
 
