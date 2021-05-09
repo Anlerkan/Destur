0957990c44bc4b307edb20b104760dcf5315babe
Anılerkan <43711824+Anlerkan@users.noreply.github.com>
2021-05-09 14:37:08 +0300

    feat(verify): Initialize

---
+++ auth/src/routes/__tests__/verify.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest';
+
+import app from '../../app';
+import { VERIFY_ROUTE } from '../route-defs';
+
+describe('tests the email verification route', () => {
+  it('should return 422 if the provided email verification token is invalid', async () => {
+    await request(app).post(VERIFY_ROUTE).send({ emailVerificationToken: 'something' }).expect(422);
+  });
+});
